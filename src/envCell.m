54917eb86cabc4f9692cb556bf56b0c6d073cf03
nicolosh <nicolo.cavalieri@studenti.unitn.it>
2024-04-09 16:21:52 +0200

    efficiently written envCell.m

--- src/envCell.m
+++ src/envCell.m
@@ -18,36 +18,31 @@ function [V, Vm, Vmap] = envCell(B, NFZ, res)
     % Vmap: array mapping motion vertices to the corresponding cells vertices.
 
     % Consider the whole rectangle containing the environment
-    sB = sort(B); % sort the elements of B
-    X = sB(1,1):res:sB(end,1);
-    Y = sB(1,2):res:sB(end,2);
-    comb = combvec(X,Y)'; % generate all combinations of the elements in vectors X and Y.
+    sortedBorders = sort(B); % sort the elements of B
+    Xrange = sortedBorders(1,1):res:sortedBorders(end,1);
+    Yrange = sortedBorders(1,2):res:sortedBorders(end,2);
+    gridPoints = combvec(Xrange, Yrange)'; % generate all combinations of the elements in vectors Xrange and Yrange
 
     % Check if inside NFZ
-    NFin = zeros(length(comb),1); % is inside a NFZ?
-    NFon = zeros(length(comb),1); % is on the boundary of NFZ?
-    for i=1:size(NFZ,2)
-        % check which points in comb are inside (in) or on the boundary (on) of the i-th NFZ. 
-        % comb(:,1) and comb(:,2) are the x and y coordinates of the points,
-        % and NFZ{i}(:,1) and NFZ{i}(:,2) are the x and y coordinates of the vertices of the i-th NFZ.
-        [in,on] = inpolygon(comb(:,1), comb(:,2), NFZ{i}(:,1), NFZ{i}(:,2));
-        NFin = sign(NFin+in); % if a point is inside the i-th NFZ, the corresponding element in NFin will be set to 1
-        NFon = sign(NFon+on); % if a point is on the boundary the i-th NFZ, the corresponding element in NFin will be set to 1
+    for i=1:length(NFZ)
+        % if a point is inside the i-th NFZ, the corresponding element in NFin will be set to 1
+        % if a point is on the boundary the i-th NFZ, the corresponding element in NFin will be set to 1
+        [NFin, NFon] = inpolygon(gridPoints(:,1), gridPoints(:,2), NFZ{i}(:,1), NFZ{i}(:,2));            % Check if each point is inside or on the border of the NFZ
     end
 
     % Check if inside Borders
-    [in,on] = inpolygon(comb(:,1), comb(:,2), B(:,1), B(:,2));
+    [in,on] = inpolygon(gridPoints(:,1), gridPoints(:,2), B(:,1), B(:,2));
 
     % Construct Vertices
     V = [];         % cells vertices           
     Vm = [];        % motion vertices
     Vmap = [];      % motion vertices map
 
-    for i = 1:length(comb)
+    for i = 1:length(gridPoints)
         if(in(i) && ~(NFin(i) - NFon(i))) % checks if the ith point is inside the region/polygon defined by B and not inside a NFZ
-            V = [V; comb(i,:)];
+            V = [V; gridPoints(i,:)];
             if(~on(i) && ~NFin(i))        % not on boundary of region and not inside a NFZ
-                Vm = [Vm; comb(i,:)];
+                Vm = [Vm; gridPoints(i,:)];
                 Vmap = [Vmap; length(V)];
             end
         end
